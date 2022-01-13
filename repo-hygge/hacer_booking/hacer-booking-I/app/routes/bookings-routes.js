7e7f4e6b1fb5cd5a0b77e7f5cc99b03a5a908280
Diana Borrajo <90630004+Dianab177@users.noreply.github.com>
2022-01-13 14:11:02 +0100

    cositas

--- repo-hygge/hacer_booking/hacer-booking-I/app/routes/bookings-routes.js
+++ repo-hygge/hacer_booking/hacer-booking-I/app/routes/bookings-routes.js
@@ -1,9 +1,12 @@
 "use strict";
 
 const express = require("express");
+const createBooking = require("../controllers/bookings/crear_reserva_controllers.js");
 
 const validateAuth = require("../middlewares/validate-auth");
 
 const router = express.Router();
+
+router.route("/").post(createBooking);
 
 module.exports = router;

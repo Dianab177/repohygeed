10df5e9c2501f044caad6a21caffc1a1282d71d9
Diana Borrajo <90630004+Dianab177@users.noreply.github.com>
2022-01-13 13:55:37 +0100

    empezar de nuevo

--- repo-hygge/denunciar-REVIEW/app/routes/users-routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const express = require("express");
-const router = express.Router();
-
-const getUserbudReviewsById = require("../controllers/users/denuncias-controllers");
-
-router.route("/id:/users").get(getUserbudReviewsById);
-
-module.exports = router;
--- repo-hygge/hacer_booking/hacer-booking-I/app/routes/bookings-routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const express = require("express");
-
-const validateAuth = require("../middlewares/validate-auth");
-
-const router = express.Router();
-
-module.exports = router;
